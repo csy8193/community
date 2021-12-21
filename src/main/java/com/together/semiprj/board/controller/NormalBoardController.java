d9db5c100524622436d8b7d09741eaf2c591bee1
park-sw <qkrtkddn77@naver.com>
2021-12-21 09:54:13 +0900

    dd

--- src/main/java/com/together/semiprj/board/controller/NormalBoardController.java
+++ src/main/java/com/together/semiprj/board/controller/NormalBoardController.java
@@ -1,6 +1,8 @@
 package com.together.semiprj.board.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -11,78 +13,101 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.together.semiprj.board.model.service.NboardReplyService;
-import com.together.semiprj.board.model.service.NboardService;
-import com.together.semiprj.board.model.vo.Nboard;
-import com.together.semiprj.board.model.vo.NboardReply;
+import com.oreilly.servlet.MultipartRequest;
+import com.together.semiprj.board.model.service.BoardService;
+import com.together.semiprj.board.model.vo.Board;
+import com.together.semiprj.board.model.vo.BoardImage;
+import com.together.semiprj.common.MyRenamePolicy;
 import com.together.semiprj.member.model.vo.Member;
 
-@WebServlet("/nboard/view")
+
+@WebServlet("/board/*")
 public class NormalBoardController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
-		//필요한 정보 set 후 foward
-		int cp = req.getParameter("cp") == null ? 1: Integer.parseInt(req.getParameter("cp"));
-		int category =  req.getParameter("category") == null ? 10: Integer.parseInt(req.getParameter("category"));
-		int boardNo = Integer.parseInt(req.getParameter("boardNo"));
-		int boardCd = Integer.parseInt(req.getParameter("boardCd"));
-		HttpSession session = req.getSession();
-		Member loginmember = (Member)session.getAttribute("loginMember");
-		int memberNo;
-		if(loginmember != null) {
-			memberNo = loginmember.getMemberNo();
-		}
-		else {
-			memberNo = 1;
-		}
-		try {
-			//2. 뷰 정보 모두 가져오기(+조회수 증가)
-			//3. 사진 가져오기
-			NboardService service = new NboardService();
-			Nboard nboard = null;
-			nboard =service.selectBoardView(boardNo,memberNo);
-			
-			//글쓴이 대표 반려동물 프로필 이미지 주소
-			String writerAnimalProfile = "";
-			if(nboard.getNboardAnimalImg() != null) {
-				if(nboard.getNboardAnimalImg().get(0).getImgLevel()==0){
-					writerAnimalProfile = (nboard.getNboardAnimalImg()).get(0).getImgPath();
-					writerAnimalProfile += (nboard.getNboardAnimalImg()).get(0).getImgName();
-				}
-			}
-			//4. 댓글정보 가져오기
-			if(nboard != null) {
-				//+ 댓글 목록 조회하기
-				List<NboardReply> rList = (new NboardReplyService()).selectReplyList(boardNo);
+		// 데이터 전달 방식 저장용 변수
+				String method = req.getMethod();
 				
-				for(NboardReply dd : rList) {
-					System.out.println(dd);
-				}
+				// 요청 주소 뒷 부분을 잘라내어 구분 방법 만들기
+				String uri = req.getRequestURI();
+				String contextPath = req.getContextPath();
 				
-				req.setAttribute("rList", rList);
-				req.setAttribute("nboard", nboard);
-				req.setAttribute("writerAnimal", writerAnimalProfile);
-				String path = "/WEB-INF/views/board/nboard-content.jsp";
-				RequestDispatcher dispatcher = req.getRequestDispatcher(path);
-				dispatcher.forward(req, resp);
-			}
-				//조회실패(게시글 없음)
-				//게시판 목록 1페이지로 이동
-				//"게시글이 존재하지 않습니다." alert
-//				절대경로
-			else{
-				session.setAttribute("message","게시글이 존재하지 않습니다.");
-				resp.sendRedirect("list");
-			}
-
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-
+				String command = uri.substring( (contextPath + "/board/").length() );
+												// /semi/board/
+				// -> 요청 주소에서 /semi/board/ 의 길이만큼 잘라낸 후 나머지 문자열을 저장
+				
+				String path = null;
+				RequestDispatcher dispatcher = null;
+				String message = null;
+				
+				try {
+					
+					BoardService service = new BoardService();
+					
+					if(command.equals("notice")) {
+						path = "/WEB-INF/views/board/notice.jsp";
+						dispatcher = req.getRequestDispatcher(path);
+						dispatcher.forward(req, resp);
+						
+					}
+					
+					
+					
+					
+					else if(command.equals("write")) {
+						path = "/WEB-INF/views/board/write.jsp";
+						dispatcher = req.getRequestDispatcher(path);
+						dispatcher.forward(req, resp);
+					}
+					
+					else if(command.equals("uploadFile")) {
+						
+						int maxSize = 1024 * 1024 * 100; // 100MB
+						
+						HttpSession session = req.getSession();
+						
+						String root = session.getServletContext().getRealPath("/");
+						
+						String filePath = "/resources/images/board/";
+						
+						String realPath = root + filePath;
+						
+						
+						MultipartRequest mReq = new MultipartRequest(req, realPath, maxSize, "UTF-8", new MyRenamePolicy());
+						
+						Enumeration<String> files = mReq.getFileNames();
+						
+						BoardImage img = new BoardImage();
+						
+						String name = files.nextElement();
+						
+						System.out.println("얻어온 name : " + name);
+						System.out.println("변환된 파일명 : " + mReq.getFilesystemName(name));
+						System.out.println("원본 파일명 : " + mReq.getOriginalFileName(name));
+						
+						resp.getWriter().print(mReq.getFilesystemName(name));
+						
+					}
+					
+					
+					
+					else if(command.equals("insert")) {
+						
+						
+						
+					}
+					
+				} catch (Exception e) {
+					
+					
+					
+				}
 	}
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
 		doGet(req, resp);
 	}
 }
